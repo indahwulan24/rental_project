77363ab0a46d425b5ea3d8e80f9d07abab50fac9
indahwulan24 <12050120353@students.uin-suska.ac.id>
2022-06-16 18:21:33 +0700

    menambahkan login

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <WelcomePage />
+  <LoginPage />
 </template>
 
 <script>
 import WelcomePage from "./components/WelcomePage/WelcomePage.vue";
+// eslint-disable-next-line no-unused-vars
+import LoginPage from "@/components/LoginPage/LoginPage";
 export default {
   name: "App",
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     WelcomePage,
+    LoginPage
   },
 };
 </script>
--- src/components/LoginPage/LoginPage.vue
+++ src/components/LoginPage/LoginPage.vue
@@ -1,9 +1,74 @@
 <template>
-  <div class="container">
-    <form></form>
+  <div class="bac">
+    <main class="form-signin grid place-items-center h-screen">
+      <div class="border p-[30px]" >
+      <form @submit.prevent="submit">
+        <h1 class="text-center font-bold border-b-2 border-b-blue-600 pb-2 mb-3">LOGIN</h1>
+
+        <div class="form-floating">
+          <label class="text-red-700 font-bold">username</label>
+          <input type="text"
+          class="form-control border-b-2 border-b-blue-600"
+          placeholder="username"/>
+        </div>
+        <div class="form-floating">
+          <label class="text-red-700 font-bold">password </label>
+          <input type="password"
+            class="form-control border-b-2 border-b-blue-600"
+            placeholder="password"/>
+        </div>
+
+        <button class="bg-sky-600 p-[10px] font-bold text-white w-full mt-3" type="submit">
+          Login
+        </button>
+
+      </form>
+      </div>
+    </main>
+
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'LoginPage',
+  components: {
+
+  }
+};
 </script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+.text-center font-bold {
+  font-size: xx-large;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin input[type="text"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+
+@media (min-width: 768px) {
+  
+}
+</style>
