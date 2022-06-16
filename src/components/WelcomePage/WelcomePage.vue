615aa7cfcf0e8db37ea4b315f83564bb607a635b
abdullahfikri <mfikri.dev@gmail.com>
2022-06-16 13:58:53 +0700

    update base v1.1

--- src/components/WelcomePage/WelcomePage.vue
+++ src/components/WelcomePage/WelcomePage.vue
@@ -1,13 +1,19 @@
 <template>
-  <navbar-page />
+  <NavbarPage />
+  <HeroPage />
+  <FooterPage />
 </template>
 
 <script>
 import NavbarPage from "./NavbarPage.vue";
+import HeroPage from "./HeroPage.vue";
+import FooterPage from "./FooterPage.vue";
 export default {
   name: "WelcomePage",
   components: {
     NavbarPage,
+    HeroPage,
+    FooterPage,
   },
 };
 </script>
